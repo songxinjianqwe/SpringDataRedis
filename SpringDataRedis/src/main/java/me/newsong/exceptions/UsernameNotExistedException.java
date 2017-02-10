a402e9dbc76fc0e096a43ba912c167a6455b842e
NJUSEGroup <songxinjianzx@163.com>
2017-02-11 01:07:30 +0800

    取出无关代码

--- SpringDataRedis/src/main/java/me/newsong/exceptions/UsernameNotExistedException.java
+++
@@ -1,8 +0,0 @@
-package me.newsong.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value=HttpStatus.NOT_ACCEPTABLE,reason="用户名不存在")
-public class UsernameNotExistedException extends RuntimeException {
-}
